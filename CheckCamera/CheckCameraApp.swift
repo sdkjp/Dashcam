1523d63bd6a0f4cf714758d7bd4c669f03b9bbe3
sdkjp <sdkblog@my.com>
2026-03-31 20:20:46 +0900

    Initial Commit

---
+++ CheckCamera/CheckCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckCameraApp.swift
+//  CheckCamera
+//
+//  Created by __s__d__k__ on 2026/03/31.
+//
+
+import SwiftUI
+
+@main
+struct CheckCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CheckCamera/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CheckCamera
+//
+//  Created by __s__d__k__ on 2026/03/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
